20b015104e41c1d72b7948e74584b99a644709aa
Aqeel Ahmed Baloch <151179923+AqeelAhmedBaloch@users.noreply.github.com>
2025-02-25 11:01:04 +0500

    Add files via upload

---
+++ Challenge2.py
@@ -0,0 +1,16 @@
+#Day-2 Challenge
+
+user_input = input("Enter Your Sentence: ")
+words = user_input.split()
+words_count = len(words)
+print(f"{user_input.upper()} Number of Words: ", words_count ,"\n")
+
+
+# Bonus Challenge (Optional)
+
+words = user_input.split()
+reverse_text = words.reverse()
+reverse_text = ' '.join(words)
+print(f"Reverse Text Is : {reverse_text}","\n")
+
+
