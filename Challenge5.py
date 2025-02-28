f8c76ae05c2da58c3399c6690b72d30cf9abe42f
Aqeel Ahmed Baloch <151179923+AqeelAhmedBaloch@users.noreply.github.com>
2025-02-28 11:45:38 +0500

    Add files via upload

---
+++ Challenge5.py
@@ -0,0 +1,10 @@
+# Day-5 Challenge
+
+# User Input
+num = int(input("Enter Any Number: "))
+
+# 1 To User Input Number Sum
+total_sum = sum(range(1, num + 1))
+
+# Result 
+print(f"Sum is: {total_sum}")
